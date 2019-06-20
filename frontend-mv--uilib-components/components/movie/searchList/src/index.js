611c028e4b899eaa1a089c03f386db9ce76db748
Pablo Rey <pabloreysobral@gmail.com>
2019-06-20 11:32:25 +0200

    Day 4 - Before create theme

--- frontend-mv--uilib-components/components/movie/searchList/src/index.js
+++ frontend-mv--uilib-components/components/movie/searchList/src/index.js
@@ -3,52 +3,54 @@ import PropTypes from 'prop-types'
 import AtomCard from '@s-ui/react-atom-card'
 import AtomImage from '@s-ui/react-atom-image'
 
-const MoviesSearchList = ({list}) => {
-  const Spinner = ({className}) => {
-    const spinnerStyles = {
-      width: '100px',
-      zIndex: 99
-    }
-
-    return (
-      <div className={className}>
-        <img
-          style={spinnerStyles}
-          src="https://zippy.gfycat.com/SkinnySeveralAsianlion.gif"
-          alt=""
-        />
-      </div>
-    )
+const Spinner = ({className}) => {
+  const spinnerStyles = {
+    width: '100px',
+    zIndex: 99
   }
 
   return (
-    <div className="mv-MovieSearch-list">
-      {list.map(item => {
-        const imageSrc =
-          item.image && `https://image.tmdb.org/t/p/w200${item.image}`
+    <div className={className}>
+      <img
+        style={spinnerStyles}
+        src="https://zippy.gfycat.com/SkinnySeveralAsianlion.gif"
+        alt=""
+      />
+    </div>
+  )
+}
+
+const MoviesSearchList = ({list, linkFactory: Link}) => {
+  return (
+    <div className="mv-MovieSearchList">
+      <header className="mv-MovieSearchList-header">List of movies</header>
+      {list.map(({id, title, image}) => {
+        const imageSrc = image && `https://image.tmdb.org/t/p/w200${image}`
         // const renderMedia = () =>
         //   imageSrc && (
         //     <img className="mv-MovieSearch-itemImage" src={imageSrc} />
         //   )
         const renderMedia = () => (
           <AtomImage
-            className="mv-MovieSearch-itemImage"
+            className="mv-MovieSearchList-itemImage"
             src={imageSrc}
             spinner={Spinner}
-            placeholder={item.title}
-            alt={`${item.title} poster`}
+            placeholder={title}
+            alt={`${title} poster`}
             errorText="Oh no!! This image couldn't be loaded"
           />
         )
-        const renderContent = () => <header>{item.title}</header>
+        const renderContent = () => <header>{title}</header>
+        const detailUrl = `/detail/${id}`
 
         return (
-          <AtomCard
-            className="mv-MovieSearch-item"
-            key={item.id}
-            media={renderMedia}
-            content={renderContent}
-          />
+          <Link href={detailUrl} key={id}>
+            <AtomCard
+              className="mv-MovieSearchList-item"
+              media={renderMedia}
+              content={renderContent}
+            />
+          </Link>
         )
       })}
     </div>
@@ -62,11 +64,13 @@ MoviesSearchList.propTypes = {
     PropTypes.shape({
       id: PropTypes.number
     })
-  )
+  ),
+  linkFactory: PropTypes.func
 }
 
 MoviesSearchList.defaultProps = {
-  list: []
+  list: [],
+  linkFactory: ({children, href}) => <a href={href}>{children}</a> //eslint-disable-line
 }
 
 export default MoviesSearchList
